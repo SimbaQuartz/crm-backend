04479bf03332b9df3f0c4ebfb86eee36bbfc0886
Amritpal Singh <amritpal.singh@simbaquartz.com>
2023-05-02 17:54:36 +0530

    country codes

--- routes/api/PlacementCell.rout.js
+++
@@ -1,8 +0,0 @@
-const express= require("express");
-const createPlacementCell = require("../../controllers/placementCell/placementCell");
-
-
-const router= express.Router();
-router.post("/add", createPlacementCell);
-
-module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -5,6 +5,7 @@ const authRoutes = require("./Auth.route");
 const validateAccessToken = require("../../middlewares/jwtValidation");
 const feedback = require("./Feedback.route.js");
 const user = require("./user.rout");
+const getCountryCode = require("../../controllers/CountryCode/CountryCode");
 const ProfileDetails = require("./ProfileDetail.route.js");
 const Contact = require("./Contact.route");
 const Enquiry = require("./Enquiry.route");
@@ -15,6 +16,7 @@ router.use("/user", user);
 router.use("/ProfileDetail", ProfileDetails);
 router.use("/contact", Contact);
 router.use("/enquiryForm", Enquiry);
+router.get("/countryCode", getCountryCode);
 
 router.get("/test", validateAccessToken, (req, res) => {
   res.status(200).json({
