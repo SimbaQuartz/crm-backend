1893a36054597c4b5dbabf10a7fcce6dbb5dd81f
Amritpal Singh <amritpal.singh@simbaquartz.com>
2023-05-08 12:00:24 +0530

    new changes

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,20 +3,14 @@ const router = require("express").Router();
 const authRoutes = require("./Auth.route");
 
 const validateAccessToken = require("../../middlewares/jwtValidation");
-const feedback = require("./Feedback.route.js");
 const user = require("./user.rout");
 const getCountryCode = require("../../controllers/CountryCode/CountryCode");
 const ProfileDetails = require("./ProfileDetail.route.js");
-const Contact = require("./Contact.route");
-const Enquiry = require("./Enquiry.route");
 const NewCase = require("./NewCase.route")
 
 router.use("/auth", authRoutes);
-router.use("/feedback", feedback);
 router.use("/user", user);
 router.use("/ProfileDetail", ProfileDetails);
-router.use("/contact", Contact);
-router.use("/enquiryForm", Enquiry);
 router.use("/newCase", NewCase);
 router.get("/countryCode", getCountryCode);
 
