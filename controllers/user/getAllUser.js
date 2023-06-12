2af42eb3dd1d2d526dbf968ae1805407c65f027e
harish chaudhary <harish.chaudhary@simbaquartz.com>
2023-06-12 16:49:04 +0530

    Updated code

--- controllers/user/getAllUser.js
+++ controllers/user/getAllUser.js
@@ -2,13 +2,35 @@ const User = require("../../models/user.model");
 
 const getAllUser = async (req, res, next) => {
   try {
-    const data = await User.find({ role: "user" });
-    const count = await User.countDocuments({ role: "user" });
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const viewSize = parseInt(req.query.viewSize) || 10;
+    const data = await User.aggregate([
+      { $match: { role: "user" } },
+      {
+        $facet: {
+          count: [{ $count: "totalCount" }],
+          data: [
+            {
+              $sort: {
+                createdAt: -1,
+              },
+            },
+
+            {
+              $skip: startIndex,
+            },
+            {
+              $limit: viewSize,
+            },
+          ],
+        },
+      },
+    ]);
     res.status(200).json({
       success: true,
       message: "User fetch successfully",
-      count: count,
-      data,
+      count: data[0].count[0].totalCount,
+      data: data[0].data,
     });
   } catch (error) {
     console.log("error", error);
